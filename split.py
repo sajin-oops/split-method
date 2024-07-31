6a3b72d7d0097c99e11a30c5014299efd7d0e226
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-31 20:40:29 +0530

    Add files via upload

---
+++ split.py
@@ -0,0 +1,12 @@
+# split method
+
+'''
+for given n,
+find a multiple of 20 greater than or
+equal to n use break in python
+'''
+str1 = "hello my name is sajin"
+emplist = str1.split(" ")
+print(emplist)
+
+#OUTPUT - ['hello', 'my', 'name', 'is', 'sajin']
